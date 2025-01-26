63bdf162639dbf3f9b65da7f1ea6217e2538d5a4
Lucas <lucas.2020109662@unicap.br>
2025-01-25 22:10:41 -0300

    Lesson 198

---
+++ 15.3+NPM/index.js
@@ -0,0 +1,8 @@
+//import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`Hello my name is ${sillyName}`);
+
+import {randomSuperhero} from "superheroes";
+const name = randomSuperhero();
+console.log(`My superhero name is ${name}`);
