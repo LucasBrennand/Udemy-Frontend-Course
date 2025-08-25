4c735e2426909129534d33dcd0b51f8d3884ac4f
Lucas Dell <lucasbrennand.barbosa@gmail.com>
2025-08-24 21:05:06 -0300

    Form handling

--- react/components/Events.jsx
+++ react/components/Events.jsx
@@ -1,36 +1,62 @@
 import { useState } from "react";
 
 function Events(params) {
-  const [title, setTitle] = useState("Hello There");
+  const [nameValue, setNameValue] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [lastNameValue, setLastNameValue] = useState("");
 
   const changeTitle = () => {
-    setTitle("General Kenobi");
+    event.preventDefault()
+    setName(nameValue);
+    setLastName(lastNameValue);
   };
 
   const changeToBlack = (event) => {
-    event.target.style.background = "black"
-  }
+    event.target.style.background = "black";
+  };
   const changeToWhite = (event) => {
-   event.target.style.background = "white"
-  }
+    event.target.style.background = "white";
+  };
+
+  const handleNameChange = (event) => {
+    setNameValue(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastNameValue(event.target.value);
+  };
 
   return (
-    <div className="h-dvh text-white flex flex-col gap-3 justify-center items-center bg-teal-800">
-      <h1 className="text-4xl">{title}</h1>
-      <input
-        type="text"
-        placeholder="What is your name?"
-        className="w-60 border-2 border-amber-200 p-2 bg-teal-950"
-      />
-      <button
-        onClick={changeTitle}
-        onMouseOver={changeToBlack}
-        onMouseLeave={changeToWhite}
-        className="w-60 border-2 border-amber-200 p-2 bg-teal-500 hover:cursor-pointer"
-      >
-        Submit
-      </button>
-    </div>
+    <form action="">
+      <div className="h-dvh text-white flex flex-col gap-3 justify-center items-center bg-teal-800">
+        <h1 className="text-4xl">
+          Hello {name} {lastName}
+        </h1>
+        <input
+          onChange={handleNameChange}
+          type="text"
+          placeholder="What is your name?"
+          className="w-60 border-2 border-amber-200 p-2 bg-teal-950"
+          value={nameValue}
+        />
+        <input
+          onChange={handleLastNameChange}
+          type="text"
+          placeholder="What is your last name?"
+          className="w-60 border-2 border-amber-200 p-2 bg-teal-950"
+          value={lastNameValue}
+        />
+        <button
+          onClick={changeTitle}
+          onMouseOver={changeToBlack}
+          onMouseLeave={changeToWhite}
+          className="w-60 border-2 border-amber-200 p-2 bg-teal-500 hover:cursor-pointer"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   );
 }
 
